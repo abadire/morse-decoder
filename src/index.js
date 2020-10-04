8bd0ddea6a24eeacc4e5bdfbe4f37ce8b9a76715
abadire <panvan97@gmail.com>
2020-10-04 22:50:28 +0300

    feat: implement decode()

--- src/index.js
+++ src/index.js
@@ -37,8 +37,30 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const TO_DURATIONS = {
+  10: '.',
+  11: '-'
+};
+
 function decode(expr) {
-    // write your solution here
+  let ret = '';
+  for (let i = 0; i < expr.length - 9; i += 10)
+  {
+    if (expr[i] === '*')
+    {
+      ret += ' ';
+      continue;
+    }
+
+    let curr = expr.slice(i, i + 10).replace(/^0+/, '');
+    let code = '';
+    for (let j = 0; j < curr.length; j += 2)
+    {
+      code += TO_DURATIONS[curr[j] + curr[j+1]];
+    }
+    ret += MORSE_TABLE[code];
+  }
+  return ret;
 }
 
 module.exports = {
